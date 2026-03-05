3595239c4e169a5bd595c63957dca9ab9b990e72
youarepreciousme-cmd <youareprecious.me@gmail.com>
2026-03-05 09:20:43 +0500

    Create Assingment Part 5 .py
    Part 5: Assignment Operators with Arithmetic

---
+++ Assingment Part 5 .py
@@ -0,0 +1,44 @@
+# 41. Initialize a variable x = 10. Use += to add 5 to it.
+x = 10
+x += 5
+print(x)
+
+# 42. Use -= to subtract 3 from a variable price.
+price = 100
+price -= 3
+print(price)
+
+# 43. Multiply a variable balance by 2 using the *= operator.
+balance = 10
+balance *= 2
+print(balance)
+
+# 44. Divide a variable total by 4 using the /= operator.
+total = 40
+total /= 4
+print(total)
+
+# 45. Use **= to square a variable.
+num = 5
+num **= 2
+print(num)
+
+# 46. Create a counter variable and increment it by 1 using assignment operators.
+counter = 999
+counter += 1
+print(counter)
+
+# 47. Use %= to find the remainder of a variable divided by 2 and update the variable.
+rem = 17
+rem %= 2
+print(rem)
+
+# 48. Use //= to perform floor division on a variable and update it.
+quotionent = 17
+quotionent //= 2
+print(quotionent)
+
+# 49. Start with n = 2. In three lines of code using assignment operators, turn it into 20.
+n = 2
+n *= 10
+print(n)
